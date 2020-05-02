5e633fc547c4b6f41f24d22c6c287b26372c57d5
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-02 23:09:10 +0530

    moved the parameter updating beforing the copy the project in github workflow

--- bin/git-workflow-symfony-basic.sh
+++ bin/git-workflow-symfony-basic.sh
@@ -5,6 +5,8 @@ composer install
 ./bin/console cache:clear;
 ./bin/console cache:clear --env=prod
 
+cat .env.local
+
 bin/console doctrine:database:create
 
 ./bin/console doctrine:schema:update --dump-sql --force
