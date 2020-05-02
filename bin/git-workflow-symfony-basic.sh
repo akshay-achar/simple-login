56d6816a0576bb8970c97842a47deafc772ef926
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-02 23:16:48 +0530

    removed the cat and ls in the github worklfow

--- bin/git-workflow-symfony-basic.sh
+++ bin/git-workflow-symfony-basic.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Composer Install
 composer install
 
+# Cache Clear Dev and Prod
 ./bin/console cache:clear;
 ./bin/console cache:clear --env=prod
 
-cat .env.local
-
+# Database Creation
 bin/console doctrine:database:create
 
+# Doctrine Schema Update
 ./bin/console doctrine:schema:update --dump-sql --force
 
+# Doctrine Cache Clear Result
 ./bin/console doctrine:cache:clear-result
