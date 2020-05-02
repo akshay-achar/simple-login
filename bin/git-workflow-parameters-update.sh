23c00ebc793b97b71eb2a2e26473408ca8caa24b
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-02 19:28:53 +0530

    updated the parameter file with the aws server mysql url

--- bin/git-workflow-parameters-update.sh
+++ bin/git-workflow-parameters-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "DATABASE_URL=mysql://root:root123@mysql_docker/auth?serverVersion=5.7" > .env.local
+echo "DATABASE_URL=mysql://root:root123@54.191.186.232:33060/auth?serverVersion=5.7" > .env.local
 
 
