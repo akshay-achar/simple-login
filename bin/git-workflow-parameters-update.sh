7be6a33124a5952ecf30f03555fb893a64a78678
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-02 19:33:15 +0530

    updated the database name

--- bin/git-workflow-parameters-update.sh
+++ bin/git-workflow-parameters-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "DATABASE_URL=mysql://root:root123@54.191.186.232:33060/auth?serverVersion=5.7" > .env.local
+echo "DATABASE_URL=mysql://root:root123@54.191.186.232:33060/auth1?serverVersion=5.7" > .env.local
 
 
