9dd876023d3a409e56091c468eb009cd65cdf07f
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-03 10:58:06 +0530

    removed the parameter updating

--- bin/git-workflow-symfony-basic.sh
+++ bin/git-workflow-symfony-basic.sh
@@ -7,8 +7,6 @@ composer install
 ./bin/console cache:clear;
 ./bin/console cache:clear --env=prod
 
-cat .env.local
-
 # Database Creation
 bin/console doctrine:database:create
 
@@ -19,5 +17,4 @@ bin/console doctrine:database:create
 ./bin/console doctrine:cache:clear-result
 
 # PHP Unit Test Running
-composer require --dev symfony/phpunit-bridge && sed -i 's/DATABASE_URL=mysql:\/\/db_user:db_password@127.0.0.1:3306\/db_name?serverVersion=5.7/DATABASE_URL=mysql:\/\/root:root123@mysql_docker\/auth?serverVersion=5.7/
-' .env.test && cat .env.test && ./bin/phpunit
+composer require --dev symfony/phpunit-bridge && ./bin/phpunit
