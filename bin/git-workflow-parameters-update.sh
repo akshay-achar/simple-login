fe4f8848b81c7a50586ac33f0fe8063337b3fdb3
Akshay Karkala <akshayachar2011@gmail.com>
2020-05-03 10:44:56 +0530

    updated the github workflow with the php unit test install

--- bin/git-workflow-parameters-update.sh
+++ bin/git-workflow-parameters-update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "DATABASE_URL=mysql://root:root123@mysql_docker/auth100?serverVersion=5.7" > .env.local
-
-echo "DATABASE_URL=mysql://root:root123@mysql_docker/auth100?serverVersion=5.7" > .env.test
+sed -i 's/DATABASE_URL=mysql:\/\/db_user:db_password@127.0.0.1:3306\/db_name?serverVersion=5.7/DATABASE_URL=mysql:\/\/root:root123@mysql_docker\/auth?serverVersion=5.7/
+' .env.local
 
--- bin/git-workflow-symfony-basic.sh
+++ bin/git-workflow-symfony-basic.sh
@@ -18,7 +18,6 @@ bin/console doctrine:database:create
 # Doctrine Cache Clear Result
 ./bin/console doctrine:cache:clear-result
 
-cat .env.test
-
 # PHP Unit Test Running
-./bin/phpunit
+composer require --dev symfony/phpunit-bridge && sed -i 's/DATABASE_URL=mysql:\/\/db_user:db_password@127.0.0.1:3306\/db_name?serverVersion=5.7/DATABASE_URL=mysql:\/\/root:root123@mysql_docker\/auth?serverVersion=5.7/
+' .env.test && cat .env.test && ./bin/phpunit
